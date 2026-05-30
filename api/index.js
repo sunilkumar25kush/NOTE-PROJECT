fd0f4d83ff1f134424c441bdb767efea06b9c756
sunilkumar25kush <sunilkumar25@navgurkul.org>
2026-05-30 17:07:51 +0530

    Fix: Improve serverless handler for Vercel

--- api/index.js
+++ api/index.js
@@ -2,8 +2,16 @@ require("dotenv").config();
 const app = require("../src/app");
 const connectdb = require("../src/db/bd");
 
-// Connect to database
-connectdb();
+// Connect to database once
+let isConnected = false;
+
+const handler = async (req, res) => {
+  if (!isConnected) {
+    await connectdb();
+    isConnected = true;
+  }
+  return app(req, res);
+};
 
 // Export for Vercel
-module.exports = app;
+module.exports = handler;
